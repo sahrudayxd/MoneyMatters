a5fe5a8e6c09470c2fa536f67a45bcd0e081de9d
sahrudayxd <gandlahoneyxd@gmail.com>
2024-01-08 20:27:04 +0530

    Add WebsiteLogo Component for Mobile

---
+++ src/components/WebsiteLogo/index.js
@@ -0,0 +1,15 @@
+import "./index.css";
+
+const WebsiteLogo = () => (
+  <div className="website-logo-container">
+    <img
+      src="https://res.cloudinary.com/dtkwvlezz/image/upload/f_auto,q_auto/v1/MoneyMatters/money-matters-logo"
+      alt="money matters logo"
+      className="website-logo-img"
+    />
+    <span className="money">Money</span>
+    <span className="matters">Matters</span>
+  </div>
+);
+
+export default WebsiteLogo;
