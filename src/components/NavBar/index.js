44ea6dfe89a8d8f30e578b65e235c614eaee37ed
sahrudayxd <gandlahoneyxd@gmail.com>
2024-01-10 01:56:00 +0530

    Add Desktop NavBar UI

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,44 +1,74 @@
 import { Component } from "react";
-import React from "react";
+import { withRouter } from "react-router-dom";
 import Popup from "reactjs-popup";
 
-import { IoMenu } from "react-icons/io5";
-import { IoCloseSharp } from "react-icons/io5";
+import { IoMenu, IoCloseSharp } from "react-icons/io5";
 
 import WebsiteLogo from "../WebsiteLogo";
 
 import "./index.css";
-// import "reactjs-popup/dist/index.css";
 
 class NavBar extends Component {
-  state = {};
+  renderMobilePopupMenu = () => (
+    <div className="mobile-menu-icon">
+      <Popup trigger={<IoMenu size={32} />} modal>
+        {(close) => (
+          <div className="mobile-menu-popup">
+            <div className="close-icon-container">
+              <button
+                className="close-menu-button"
+                type="button"
+                onClick={close}
+              >
+                <IoCloseSharp size={32} />
+                {` `}
+              </button>
+            </div>
+
+            <h1>Menu Popup</h1>
+          </div>
+        )}
+      </Popup>
+    </div>
+  );
+
+  renderDesktopHeaderView = () => {
+    const { match } = this.props;
+    const { path } = match;
+    console.log(path);
+    const navbarContent = () => {
+      switch (path) {
+        case "/dashboard":
+          return "Dashboard";
+        case "/transactions":
+          return "Transactions";
+        case "/account":
+          return "Account";
+        default:
+          return "";
+      }
+    };
+
+    return (
+      <div className="navbar-desktop-header">
+        <h1 className="navbar-title">{navbarContent()}</h1>
+        <button className="add-transaction-button">+ Add Transaction</button>
+      </div>
+    );
+  };
 
   render() {
     return (
       <nav className="nav">
-        <WebsiteLogo />
+        <div className="nav-logo">
+          <WebsiteLogo />
+        </div>
 
-        <Popup trigger={<IoMenu size={32} />} modal>
-          {(close) => (
-            <div className="mobile-menu-popup">
-              <div className="close-icon-container">
-                <button
-                  className="close-menu-button"
-                  type="button"
-                  onClick={close}
-                >
-                  <IoCloseSharp size={32} />
-                  {` `}
-                </button>
-              </div>
-
-              <h1>Menu Popup</h1>
-            </div>
-          )}
-        </Popup>
+        {this.renderMobilePopupMenu()}
+        {this.renderDesktopHeaderView()}
       </nav>
     );
   }
 }
 
-export default NavBar;
+export default withRouter(NavBar);
