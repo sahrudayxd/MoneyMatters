1a5096f79a07fbf6f61dec82a7c1c156655c35c1
sahrudayxd <gandlahoneyxd@gmail.com>
2024-01-26 22:31:57 +0530

    Update NotFound Component

--- src/components/NotFound/index.js
+++ src/components/NotFound/index.js
@@ -1,20 +1,24 @@
-import { Component } from "react";
-
-import SidebarOptions from "../SidebarOptions";
-
+import { Link } from "react-router-dom";
+import Cookies from "js-cookie";
 import "./index.css";
 
-class NotFound extends Component {
-  render() {
-    return (
-      <div className="not-found">
-        <div className="desktop-sidebar">
-          <SidebarOptions />
-        </div>
-        NotFound
-      </div>
-    );
-  }
-}
+const NotFound = () => {
+  const userId = Cookies.get("money_matters_id");
+
+  return (
+    <div className="not-found-container">
+      <h1 className="not-found-title">Lost Your Way ?</h1>
+      <p className="not-found-msg">
+        We are sorry, the page you requested could not be found Please go back
+        to the homepage.
+      </p>
+      <Link to="/dashboard">
+        <button type="button" className="not-found-button">
+          {userId === undefined ? "Sign in" : "Go To Dashboard"}
+        </button>
+      </Link>
+    </div>
+  );
+};
 
 export default NotFound;
